93d3be570a08779bf59f38f404aa9316a8102fbd
shivamkumarpandit055-code <shivamkumarpandit055@gmail.com>
2025-08-13 22:23:46 +0530

    Update saini.py

--- saini.py
+++ saini.py
@@ -245,7 +245,7 @@ async def download_video(url,cmd, name):
         elif os.path.isfile(f"{name}.webm"):
             return f"{name}.webm"
         name = name.split(".")[0]
-        if os.path.isfile(f"{name}.mkv"):
+        if os.path.isfile(f"{name}.mp4"):
             return f"{name}.mkv"
         elif os.path.isfile(f"{name}.mp4"):
             return f"{name}.mp4"
